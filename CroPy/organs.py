c5105e0a027f49aeaae5eb1b9c3db808736d1e8f
Cody Markelz <rjmarkelz@ucdavis.edu>
2015-11-29 22:49:45 -0800

    documentation, all tests passing, organ class outline

--- CroPy/organs.py
+++ CroPy/organs.py
@@ -1,26 +1,44 @@
 class Organ(object):
     '''
-    doc tests are up next
+    This class makes an organ with X, Y, Z coordinates in a 3D space,
+    it will calculate C, N, and P demand based on the size and 
+    developmental stage of the organ.
     '''
 
-    def __init__(self, name='', shape=[]):
+    def __init__(self, name):
         self.name = name
-        self.shape = shape
+        # self.size = 1
+        # self.xcoord = 0
+        # self.ycoord = 0
+        # self.zcoord = 0
+
+    def space(self):
+        print("Space")
+
+    def c_demand(self):
+        print("C demand")
+
+    def n_demand(self):
+        print("N demand")
+
+    def p_demand(self):
+        print("P demand")
     # size
     # X
     # Y
     # Z
-    # C
-    # N
 
 class Leaf(Organ):
     '''
+    This class is a leaf. It has special methods to calculate photosynthesis
+    and respiration. 
     '''
     class_counter = 0
-    def __init__(self, name='', shape='', area=1):
-        Organ.__init__(self, name, shape)
+    def __init__(self, name='', shape=10, area=1):
+        # super().__init__(self, name)
         self.area = area
         self.rate = 10
+        self.shape = shape
         self.id = Leaf.class_counter
         Leaf.class_counter += 1
 
@@ -29,3 +47,5 @@ class Leaf(Organ):
 
     def respiration(self):
         return (self.area * self.rate) / 2
+
+
